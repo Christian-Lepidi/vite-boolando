54f0ac3c3f6c5169dc73f6edab38315f75a89604
Christian-Lepidi <christianlepidi@gmail.com>
2024-02-08 16:30:17 +0100

    feat: failure alignment

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -3,7 +3,46 @@ export default {};
 </script>
 
 <template>
-  <footer></footer>
+  <footer class="footer">
+    <div class="footer-container">
+      <div>
+        <h4 class="h4">Boolando s.r.l.</h4>
+        <a href="#" class="link">infomazioni legali</a>
+        <a href="#" class="link">infomativa privacy</a>
+        <a href="#" class="link">diritto di recesso</a>
+      </div>
+      <span class="info">Trovaci anche su</span>
+    </div>
+  </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.footer {
+  width: 100%;
+  height: 120px;
+  background-color: rgb(34, 34, 34);
+
+  .footer-container {
+    width: 80%;
+    height: 120px;
+    margin: auto;
+  }
+
+  @include alignment();
+  .h4 {
+    font-size: 18px;
+    color: white;
+    margin-bottom: 5px;
+  }
+  .link {
+    text-decoration: none;
+    color: white;
+  }
+
+  .info {
+    font-size: 18px;
+    color: white;
+    margin-right: 0;
+  }
+}
+</style>
--- src/AppHeader.vue
+++ src/AppHeader.vue
@@ -3,7 +3,48 @@ export default {};
 </script>
 
 <template>
-  <header></header>
+  <header class="header">
+    <div class="header-container">
+      <span class="text-header">donna uomo bambini</span>
+      <img src="../cartella slack/img/boolean-logo.png" alt="" class="logo" />
+      <div class="icons-header">
+        <i class="far fa-user"></i>
+        <i class="far fa-heart"></i>
+        <i class="fas fa-shopping-bag"></i>
+      </div>
+    </div>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  width: 100%;
+  height: 90px;
+  background-color: orangered;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+}
+
+.header-container {
+  width: 80%;
+  height: 90px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text-header {
+  font-size: 20px;
+  color: white;
+}
+
+.logo {
+  height: 40px;
+}
+
+.icons-header {
+  color: white;
+}
+</style>
