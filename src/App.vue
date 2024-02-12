ad72610a0e9e2a3ac15660f7c35712c04547ad9a
Christian-Lepidi <christianlepidi@gmail.com>
2024-02-12 17:03:49 +0100

    feat : add. wrong code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script>
+import axios from "axios";
+
 import AppHeader from "./AppHeader.vue";
 import AppMain from "./AppMain.vue";
 import AppFooter from "./AppFooter.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      products: [],
+    };
   },
 
   components: { AppHeader, AppMain, AppFooter },
+
+  created() {
+    axios.get("http://localhost:3000/products").then((resp) => {
+      this.products = resp.data;
+    });
+  },
 };
 </script>
 
